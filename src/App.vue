4b2a3404fa67cde41283114755dcfd1adea3b621
xuxiaoyin <279238850@qq.com>
2018-11-18 02:06:17 +0800

    header

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+  </div>
+</template>
+
+<script>
+import VHeader from 'components/v-header/v-header'
+import {getSeller} from 'api'
+
+export default {
+  data(){
+    return {
+      seller: {}
+    }
+  },
+  created(){
+    getSeller().then( (seller) =>{
+      this.seller=seller
+    })
+  },
+  components:{
+    VHeader
+  }
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
